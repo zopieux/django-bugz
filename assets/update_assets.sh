53abd92d097552c0146b52c265daaaef7c4bf5ac
Antoine Pietri <antoine.pietri1@gmail.com>
2020-07-18 15:40:01 +0200

    assets: rename from js/ and build with script

---
+++ assets/update_assets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$( dirname $0 )"
+
+dest="../bugz/static/bugz/"
+mkdir -p "$dest"
+
+yarn build
+
+cat \
+    build/static/js/runtime*js \
+    build/static/js/2*js \
+    build/static/js/main*js \
+    > "$dest/bundle.js"
